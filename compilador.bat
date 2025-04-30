444b6df40e0454575311d5feff2dbef7b8627c95
Renato Chiari <renatochiari_15@hotmail.com>
2025-04-30 17:53:01 -0300

    Ajustes no bat de compilação

--- compilador.bat
+++ compilador.bat
@@ -1,2 +1,2 @@
 call "C:\Program Files (x86)\Embarcadero\Studio\21.0\bin\rsvars.bat"
-msbuild CalculatorCICD.dproj
+msbuild CalculatorCICD.dproj /target:build /p:Config=Release  /p:DCC_UseMSBuildExternally=true /p:DCC_BuildAllUnits=true /p:Platform=Win32  /clp:ErrorsOnly
